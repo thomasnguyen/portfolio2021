4bd95d1132152f049e6d4a8d5bfb30c7b7a2876e
thomasnguyen <imtoshiru@gmail.com>
2020-12-24 10:26:09 -0800

    Implemented Header component

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,117 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  margin: 3em 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+  font-size: 1em;
+  @media (max-width: 768px) {
+    margin: 2.5em 0 0 0;
+    padding: 0 3em;
+    justify-content: space-around;
+    & > :first-child {
+      margin-bottom: 1.5em;
+    }
+  }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Avatar2 = styled.img`
+  width: 7em;
+  height: 7em;
+  background: pink;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 1.5em 0 0;
+  border-radius: 50%;
+  background-image: url("/ac_avatar.png");
+  background-position: center;
+  background-size: 7em 7em;
+`;
+
+const Avatar = styled.img`
+  width: 5em;
+  height: 5em;
+  margin: 0 1.5em 0 0;
+  border-radius: 50%;
+`;
+
+const LogoText = styled.div`
+  h1 {
+    font-size: 1.8em;
+    font-weight: 500;
+    margin: 0.1em 0;
+  }
+  h2 {
+    font-size: 1.2em;
+    font-weight: 300;
+    margin: 0;
+    padding: 0 0 0 0.2em;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  & > a {
+    font-size: 1.5em;
+    margin-right: 2.5em;
+    border-bottom: dashed 0.1em rgba(0, 0, 0, 0.4);
+    padding-bottom: 0.2em;
+    transition: border-bottom 500ms ease-in-out;
+    text-decoration: none;
+    color: black;
+    &:hover {
+      cursor: pointer;
+      border-bottom: solid 0.1em rgba(0, 0, 0, 0.4);
+    }
+    font-weight: 600;
+  }
+`;
+
+const L = styled.div`
+  font-weight: 600;
+  cursor: pointer;
+  margin-left: 1.5em;
+  font-size: 1.1em;
+  opacity: 0.9;
+  &:hover {
+    color: #688624;
+    text-decoration: underline dotted;
+  }
+`;
 
 export default function Header() {
   return (
     <>
-      <header className="header">
-        <nav className="nav">
+      <HeaderContainer>
+        <Logo>
+          <Avatar src="/ac_avatar.png" />
+          <LogoText>
+            <h1>Thomas Nguyen</h1>
+            <h2>Engineering a better humanity.</h2>
+          </LogoText>
+        </Logo>
+        <Nav>
           <Link href="/">
-            <a>My Blog</a>
+            <L>Home</L>
           </Link>
-          <Link href="/about">
-            <a>About</a>
+          <Link href="/blog">
+            <L>Blog</L>
           </Link>
-        </nav>
-      </header>
+        </Nav>
+      </HeaderContainer>
     </>
   );
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,22 @@
 import Head from "next/head";
+import styled from "styled-components";
+
 import Header from "./Header";
+
+const Section = styled.section`
+  width: 100%;
+  max-width: 1100px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 32px;
+  padding-right: 32px;
+  position: sticky;
+  top: 0px;
+  z-index: 2;
+
+  font-size: 12px;
+  font-family: "Inter", sans-serif;
+`;
 
 export default function Layout({ children, pageTitle, ...props }) {
   return (
@@ -7,11 +24,17 @@ export default function Layout({ children, pageTitle, ...props }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{pageTitle}</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300,500,600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <section className="layout">
+      <Section>
+        <Header />
         <Header />
         <div className="content">{children}</div>
-      </section>
+      </Section>
       <footer>Built by me!</footer>
     </>
   );
